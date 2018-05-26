860e82a436f689622e133d3abd517b6a68823584
voyager.woo <voyager.woo@gmail.com>
2018-05-27 01:17:17 +0900

    마크다운 추가

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,33 @@
 <template>
   <section>
-      <h2>About</h2>
-      I am voyager.
+    <div class="markdown-wrap">
+      <vue-markdown v-if="this.source">{{this.source}}</vue-markdown>
+    </div>
   </section>
 </template>
+
+<script>
+import VueMarkdown from 'vue-markdown'
+import readMarkdown from '../utils/readMarkdown'
+
+export default {
+  data () {
+    return {
+      source: ''
+    }
+  },
+  components: {
+    VueMarkdown
+  },
+  methods: {},
+  created () {
+    readMarkdown('about').then(r => {
+      this.source = r
+      return r
+    })
+  }
+}
+</script>
+
+<style>
+</style>
